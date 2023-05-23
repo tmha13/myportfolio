6c6e8bdb9d7d8f71f0b5e1fb142cdc24c2ae8498
Trinh Ha <trinhmanhha@email.arizona.edu>
2023-05-23 13:22:53 -0700

    Update About Page

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,8 +12,9 @@ export default function About() {
         <div className="about-me">
           <ul>
             <li>An international student from Vietnam. I graduated from University of Arizona at the end of 2021 with a BS degree</li>
+            <li>Currently pursuing a degree of cyber security in Pima Community College to broaden my knowledge</li>
             <li>I'm confident writing codes in Java, Javascript and C++. I have experiences with Nodejs, Expressjs, SQL, Mongoose for MongoDB, and React. 
-              I'm currently focusing on React to polish my front-end skill.</li>
+              I'm currently focusing on self-teaching React to polish my front-end skill.</li>
             <li>I consider myself a minimalist, and I believe this portfolio of mine does a good job showing that.</li>
             <li>Outside of coding, I take great interest in card games, whether it be playing with cards or collecting them!
               I used to spend a lot of time on TCGs like YGO or Pokemon. Nowadays, I work on my own card game along with my group of friends.
